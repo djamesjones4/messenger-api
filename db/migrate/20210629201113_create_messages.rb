2d5f3d2289fa42c26599febf4d9730164948f3de
Derek Jones <derek.jones@singlebrook.com>
2021-06-29 14:51:27 -0600

    install and initial scaffolding

---
+++ db/migrate/20210629201113_create_messages.rb
@@ -0,0 +1,12 @@
+class CreateMessages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :messages do |t|
+      t.string :content, null: false
+      t.references :to_user, null: false, foreign_key: { to_table: :users }
+      t.references :from_user, null: false, foreign_key: { to_table: :users }
+      t.boolean :read, default: false
+
+      t.timestamps
+    end
+  end
+end

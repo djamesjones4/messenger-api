b7d7ddc7740bc32445b3ba8a1651376ec1607dfb
Derek Jones <derek.jones@singlebrook.com>
2021-06-30 09:39:36 -0600

    destroyed conversation model

--- db/migrate/20210630144707_create_conversations.rb
+++
@@ -1,9 +0,0 @@
-class CreateConversations < ActiveRecord::Migration[6.1]
-  def change
-    create_table :conversations do |t|
-      t.integer :participants, array: true, null: false, default: []
-
-      t.timestamps
-    end
-  end
-end

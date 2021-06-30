077809cdeb6171357282cd340ff243175e6917b4
Derek Jones <derek.jones@singlebrook.com>
2021-06-30 10:00:38 -0600

    updated foreign key info

--- db/migrate/20210630155044_create_messages.rb
+++ db/migrate/20210630155044_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
       t.string :content
-      t.references :conversation, null: false, foreign_key: true
-      t.references :from_user, null: false, foreign_key: true
+      t.references :conversation, null: false, foreign_key: { to_table: :conversations }
+      t.references :from_user, null: false, foreign_key: { to_table: :users }
       t.boolean :read
 
       t.timestamps

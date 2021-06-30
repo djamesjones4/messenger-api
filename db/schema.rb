ca06d042984f94c16f42d45868a552678a1f37a9
Derek Jones <derek.jones@singlebrook.com>
2021-06-30 09:17:16 -0600

    migrated dropping of tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_201113) do
+ActiveRecord::Schema.define(version: 2021_06_30_151341) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "participants", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "content", limit: 160, null: false

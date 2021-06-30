f4bc31438e12c421607a06993fe5edc842b41f08
Derek Jones <derek.jones@singlebrook.com>
2021-06-30 11:59:27 -0600

    updated participants default

--- db/migrate/20210630154411_create_conversations.rb
+++ db/migrate/20210630154411_create_conversations.rb
@@ -2,7 +2,7 @@ class CreateConversations < ActiveRecord::Migration[6.1]
   def change
     drop_table('conversations')
     create_table :conversations do |t|
-      t.integer :participants, array: true, default: []
+      t.integer :participants, array: true, null: false
 
       t.timestamps
     end

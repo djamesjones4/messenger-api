cf3a8a3948a9c7f3143f58642fcb081838f9fee9
Derek Jones <derek.jones@singlebrook.com>
2021-06-30 08:51:14 -0600

    updated participants to be array of integers

--- db/migrate/20210630144707_create_conversations.rb
+++ db/migrate/20210630144707_create_conversations.rb
@@ -1,7 +1,7 @@
 class CreateConversations < ActiveRecord::Migration[6.1]
   def change
     create_table :conversations do |t|
-      t.integer :participants
+      t.integer :participants, array: true, null: false, default: []
 
       t.timestamps
     end

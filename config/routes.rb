0acac1da1bde0bceac9b85fdb94b47464427afd7
Derek Jones <derek.jones@singlebrook.com>
2021-06-30 20:50:38 -0600

    added route to get messages from a specific user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  #TODO: add route for getting by from_user_id
-  # get '/messages/:from_user_id', to: messages#getFromUser
+  # get messages from a specific user based on conversation ID
+  get '/by_conversation/', to: 'messages#getFromUser'
 end
